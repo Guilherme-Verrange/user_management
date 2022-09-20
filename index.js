fc4c172ab57da7ae9baff3f36d3c53a155145f78
Guilherme-Verrange <verrangeguilherme@gmail.com>
2022-09-20 11:40:34 -0300

    Added condition structure (If and Else) to identify if form gender is checked.

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ var fields = document.querySelectorAll("#form-user-create [name]");
 
 fields.forEach(function(field, index){
 
-    console.log(field.id, field.name, field.value, field.checked, index);
+    if (field.name == "gender"){
 
+        if (field.checked){
+            console.log("SIm", field);
+        }
 
+    }else{
+
+        console.log("Não");
+
+    }
+
+   
 });
