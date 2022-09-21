61ce34ec379661773d4282b35f3303aa3e9e2c13
Guilherme-Verrange <verrangeguilherme@gmail.com>
2022-09-21 12:58:22 -0300

    Error handling done when loading an empty photo. Added a ternary operator to check if admin_user is true or false. Now when creating a user it will not overwrite the current one.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -59,10 +59,14 @@ class UserController{
 
         fileReader.onerror = (e) =>{
             reject(e);
+        };
+
+        if (file) {
+            fileReader.readAsDataURL(file);
+        } else{
+
+            resolve('/dist/img/avatar.png');
         }
-        fileReader.readAsDataURL(file);
-
-
 
         });
     }
@@ -80,6 +84,10 @@ class UserController{
                   user[field.name] = field.value;
                 }
         
+            }else if(field.name == "admin"){
+                
+                user[field.name] = field.checked; // Verifica o estado do checked admin.
+                
             }else{
         
                 user[field.name] = field.value;
@@ -97,24 +105,23 @@ class UserController{
 
 
      addLine(dataUser){
-    
-        this.tableElement.innerHTML = `
         
-        <tr>
-            <td>
-            <img src=${dataUser.photo} alt="User Image" class="img-circle img-sm">
-            </td>
+        let tr = document.createElement('tr');
+
+        tr.innerHTML = `
+        
+            <td><img src=${dataUser.photo} alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
+                <td>${(dataUser.admin) ? "Sim" : "Não"}</td>
                 <td>${dataUser.birth}</td>
-                <td>
+            <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
-         </tr>
-        
         `;
+
+        this.tableElement.appendChild(tr);
     }
 
 
