d3401339c5b37c0fe95e0a826ca5d1752ff1850a
Guilherme-Verrange <verrangeguilherme@gmail.com>
2022-09-23 10:42:24 -0300

    Change from sessionStore to localStore

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -205,9 +205,9 @@ class UserController{
 
         let users = [];
 
-        if (sessionStorage.getItem("users")) {
+        if (localStorage.getItem("users")) {
 
-            users = JSON.parse(sessionStorage.getItem("users"));
+            users = JSON.parse(localStorage.getItem("users"));
 
         }
 
@@ -237,7 +237,7 @@ class UserController{
 
         users.push(data);
 
-        sessionStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem("users", JSON.stringify(users));
 
     }
 
