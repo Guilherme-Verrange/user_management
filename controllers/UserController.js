bbb8fb152e1abda99331af2b6dc1cd669ffb10a5
Guilherme-Verrange <verrangeguilherme@gmail.com>
2022-09-23 09:20:32 -0300

    Added the function to edit the user's image. Now it is possible to perform the exchange without losing the file in the update.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -33,25 +33,48 @@ class UserController{
 
             let tr = this.tableElement.rows[index];
 
-            tr.dataset.user = JSON.stringify(values);
+            let userOld = JSON.parse(tr.dataset.user);
 
-            tr.innerHTML = `
-                <tr>
-                    <td><img src=${values.photo} class="img-circle img-sm"></td>
-                    <td>${values.name}</td>
-                    <td>${values.email}</td>
-                    <td>${(values.admin) ? 'Sim' : 'Não'}</td>
-                    <td>${Utils.dateFormat(values.register)}</td>
-                    <td>
-                        <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
-                        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-                    </td>
-                </tr>
-            `;
+            let result = Object.assign({}, userOld, values);
 
-            this.addEventsTr(tr);
+            this.getPhoto(this.formUpdateElement).then(
+                (content) => {
 
-            this.updateCount();
+                    if (!values.photo){ 
+                        result._photo = userOld._photo;
+                    } else {
+                        result._photo = content;
+                    }
+
+                    tr.dataset.user = JSON.stringify(result);
+
+                    tr.innerHTML = `
+                        <td><img src="${result._photo}" class="img-circle img-sm"></td>
+                        <td>${result._name}</td>
+                        <td>${result._email}</td>
+                        <td>${(result._admin) ? 'Sim' : 'Não'}</td>
+                        <td>${Utils.dateFormat(result._register)}</td>
+                        <td>
+                            <button type="button" class="btn btn-primary btn-edit btn-xs btn-flat">Editar</button>
+                            <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+                        </td>
+                    `;
+
+                    this.addEventsTr(tr);
+
+                    this.updateCount();
+
+                    this.formUpdateElement.reset();
+            
+                    this.showPanelCreate();
+
+                    btn.disabled = false;
+
+                }, 
+                (e) => {
+                    console.error(e)
+                }
+            );
 
         });
 
@@ -71,7 +94,7 @@ class UserController{
 
             let values = this.getValues(this.formElement);
 
-            this.getPhoto().then
+            this.getPhoto(this.formElement).then
             ((content) => {
                 values.photo = content;
 
@@ -92,42 +115,42 @@ class UserController{
     } // class for to take submit function
 
 
-    getPhoto(){
+    getPhoto(formElement){
 
-        return new Promise((resolve, eject) => {
-
+        return new Promise((resolve, reject) => {
 
             let fileReader = new FileReader();
 
-        let elements = [...this.formElement.elements].filter(item => {
+            let elements = [...formElement.elements].filter(item => {
 
-            if (item.name === 'photo') {
-                return item;
+                if (item.name === 'photo') {
+                    return item;
+                }
+
+            });
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = () => {
+
+                resolve(fileReader.result);
+
+            };
+
+            fileReader.onerror = (e) => {
+
+                reject(e);
+
+            };
+
+            if(file) {
+                fileReader.readAsDataURL(file);
+            } else {
+                resolve('dist/img/avatar.png');
             }
 
         });
 
-        let file = elements[0].files[0];
-
-        fileReader.onload = () => {
-
-            resolve(fileReader.result);
-
-        };
-
-
-        fileReader.onerror = (e) =>{
-            reject(e);
-        };
-
-        if (file) {
-            fileReader.readAsDataURL(file);
-        } else{
-
-            resolve('/dist/img/avatar.png');
-        }
-
-        });
     }
 
     getValues(formElement){
@@ -208,13 +231,13 @@ class UserController{
         tr.querySelector(".btn-edit").addEventListener("click", e => {
 
             let json = JSON.parse(tr.dataset.user);
-            let form = document.querySelector("#form-user-update");
+            
 
-            form.dataset.trIndex = tr.sectionRowIndex;
+            this.formUpdateElement.dataset.trIndex = tr.sectionRowIndex;
 
             for (let name in json) {
 
-                let field = form.querySelector("[name=" + name.replace("_", "") + "]");
+                let field = this.formUpdateElement.querySelector("[name=" + name.replace("_", "") + "]");
 
                 if (field) {
 
@@ -224,7 +247,7 @@ class UserController{
                             break;
                             
                         case 'radio':
-                            field = form.querySelector("[name=" + name.replace("_", "") + "][value=" + json[name] + "]");
+                            field = this.formUpdateElement.querySelector("[name=" + name.replace("_", "") + "][value=" + json[name] + "]");
                             field.checked = true;
                         break;
 
@@ -242,6 +265,8 @@ class UserController{
 
 
             }
+
+            this.formUpdateElement.querySelector(".photo").src = json._photo;
             
             this.showPanelUpdate();
 
